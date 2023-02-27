e6924c9a84cadc54e30ae3e671e2fd2a85603614
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-02-27 17:19:09 +0300

    I learned handleSubmit & handleChange

--- src/App.js
+++ src/App.js
@@ -1,5 +1,56 @@
+// import "./App.css";
+// import { Formik, Field, Form } from "formik";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Sign Up</h1>
+//       <Formik
+//         initialValues={{
+//           firstName: "",
+//           lastName: "",
+//           email: "",
+//         }}
+//         onSubmit={(values) => {
+//           console.log(values);
+//         }}
+//       >
+//         <Form>
+//           <label htmlFor="firstName">First Name</label>
+//           <Field id="firstName" name="firstName" placeholder="Jane" />
+
+//           <br />
+//           <br />
+
+//           <label htmlFor="lastName">Last Name</label>
+//           <Field id="lastName" name="lastName" placeholder="Doe" />
+
+//           <br />
+//           <br />
+
+//           <label htmlFor="email">Email</label>
+//           <Field
+//             id="email"
+//             name="email"
+//             placeholder="jane@acme.com"
+//             type="email"
+//           />
+
+//           <br />
+//           <br />
+
+//           <button type="submit">Submit</button>
+//         </Form>
+//       </Formik>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//handleSubmit & handleChange
 import "./App.css";
-import { Formik, Field, Form } from "formik";
+import { Formik } from "formik";
 
 function App() {
   return (
@@ -15,33 +66,30 @@ function App() {
           console.log(values);
         }}
       >
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="Jane" />
+        {({ handleSubmit, handleChange }) => (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input name="firstName" onChange={handleChange} />
 
-          <br />
-          <br />
+            <br />
+            <br />
 
-          <label htmlFor="lastName">Last Name</label>
-          <Field id="lastName" name="lastName" placeholder="Doe" />
+            <label htmlFor="lastName">Last Name</label>
+            <input name="lastName" onChange={handleChange} />
 
-          <br />
-          <br />
+            <br />
+            <br />
 
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="jane@acme.com"
-            type="email"
-          />
+            <label htmlFor="email">Email</label>
+            <input name="email" onChange={handleChange} />
 
-          <br />
-          <br />
+            <br />
+            <br />
 
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Formik>{" "}
     </div>
   );
 }
