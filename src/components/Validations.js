ebef7794907f5a9770cb6dcf9c23c6c742417530
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-02-28 16:18:56 +0300

    error messages are customized

--- src/components/Validations.js
+++ src/components/Validations.js
@@ -1,11 +1,13 @@
 import { object, string, number, date, InferType, ref } from "yup";
 
 const Validations = object({
-  email: string().email().required(),
-  password: string().min(5).required,
+  email: string().email("Geçerli bir email girin.").required("Zorunlu alan."),
+  password: string()
+    .min(5, "Parolanız en az 5 karakter olmalıdır.")
+    .required("Zorunlu alan."),
   passwordConfirm: string()
-    .oneOf([ref("password")])
-    .required(),
+    .oneOf([ref("password")], "Parolalar uyuşmuyor.")
+    .required("Zorunlu alan."),
 });
 
 export default Validations;
