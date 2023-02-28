a6d635ef90c52bd6574c597fe03f57f643c646b3
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-02-28 13:42:34 +0300

    Form Validations-1 started

--- src/App.js
+++ src/App.js
@@ -159,18 +159,123 @@
 // export default App;
 
 //useFormik
+// import "./App.css";
+// import { useFormik } from "formik";
+
+// function App() {
+//   const { handleSubmit, handleChange } = useFormik({
+//     initialValues: {
+//       firstName: "",
+//       lastName: "",
+//       email: "",
+//       gender: "",
+//       hobies: [],
+//       country: "",
+//     },
+//     onSubmit: (values) => {
+//       console.log(values);
+//     },
+//   });
+//   return (
+//     <div className="App">
+//       <h1>Sign Up</h1>
+
+//       <form onSubmit={handleSubmit}>
+//         <label htmlFor="firstName">First Name</label>
+//         <input name="firstName" onChange={handleChange} />
+
+//         <br />
+//         <br />
+
+//         <label htmlFor="lastName">Last Name</label>
+//         <input name="lastName" onChange={handleChange} />
+
+//         <br />
+//         <br />
+
+//         <label htmlFor="email">Email</label>
+//         <input name="email" onChange={handleChange} />
+
+//         <br />
+//         <br />
+
+//         <span>Male</span>
+//         <input
+//           type="radio"
+//           name="gender"
+//           value="male"
+//           onChange={handleChange}
+//         />
+
+//         <span>Female</span>
+//         <input
+//           type="radio"
+//           name="gender"
+//           value="female"
+//           onChange={handleChange}
+//         />
+
+//         <br />
+//         <br />
+
+//         <div>
+//           <input
+//             type="checkbox"
+//             name="hobies"
+//             value="football"
+//             onChange={handleChange}
+//           />
+//           Football
+//         </div>
+//         <div>
+//           <input
+//             type="checkbox"
+//             name="hobies"
+//             value="cinema"
+//             onChange={handleChange}
+//           />
+//           Cinema
+//         </div>
+//         <div>
+//           <input
+//             type="checkbox"
+//             name="hobies"
+//             value="photography"
+//             onChange={handleChange}
+//           />
+//           Photography
+//         </div>
+
+//         <br />
+//         <br />
+
+//         <select name="country" onChange={handleChange}>
+//           <option value="TR">Turkey</option>
+//           <option value="EN">England</option>
+//           <option value="USA">USA</option>
+//         </select>
+
+//         <br />
+//         <br />
+
+//         <button type="submit">Submit</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//Form Validasyonları
 import "./App.css";
 import { useFormik } from "formik";
 
 function App() {
   const { handleSubmit, handleChange } = useFormik({
     initialValues: {
-      firstName: "",
-      lastName: "",
       email: "",
-      gender: "",
-      hobies: [],
-      country: "",
+      password: "",
+      passwordConfirm: "",
     },
     onSubmit: (values) => {
       console.log(values);
@@ -181,79 +286,20 @@ function App() {
       <h1>Sign Up</h1>
 
       <form onSubmit={handleSubmit}>
-        <label htmlFor="firstName">First Name</label>
-        <input name="firstName" onChange={handleChange} />
-
-        <br />
-        <br />
-
-        <label htmlFor="lastName">Last Name</label>
-        <input name="lastName" onChange={handleChange} />
-
-        <br />
-        <br />
-
         <label htmlFor="email">Email</label>
         <input name="email" onChange={handleChange} />
 
         <br />
         <br />
 
-        <span>Male</span>
-        <input
-          type="radio"
-          name="gender"
-          value="male"
-          onChange={handleChange}
-        />
-
-        <span>Female</span>
-        <input
-          type="radio"
-          name="gender"
-          value="female"
-          onChange={handleChange}
-        />
+        <label htmlFor="password">Password</label>
+        <input name="password" onChange={handleChange} />
 
         <br />
         <br />
 
-        <div>
-          <input
-            type="checkbox"
-            name="hobies"
-            value="football"
-            onChange={handleChange}
-          />
-          Football
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            name="hobies"
-            value="cinema"
-            onChange={handleChange}
-          />
-          Cinema
-        </div>
-        <div>
-          <input
-            type="checkbox"
-            name="hobies"
-            value="photography"
-            onChange={handleChange}
-          />
-          Photography
-        </div>
-
-        <br />
-        <br />
-
-        <select name="country" onChange={handleChange}>
-          <option value="TR">Turkey</option>
-          <option value="EN">England</option>
-          <option value="USA">USA</option>
-        </select>
+        <label htmlFor="passwordConfirm">Confirm Password</label>
+        <input name="passwordConfirm" onChange={handleChange} />
 
         <br />
         <br />
