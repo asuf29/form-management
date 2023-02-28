6c8aee8c7b7f272083a67188fa24a64a18e702cf
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-02-28 16:02:57 +0300

    error message given

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import { useFormik } from "formik";
 import validationSchema from "./Validations";
 function Signup() {
-  const { handleSubmit, handleChange } = useFormik({
+  const { handleSubmit, handleChange, errors } = useFormik({
     initialValues: {
       email: "",
       password: "",
@@ -12,6 +12,7 @@ function Signup() {
     },
     validationSchema,
   });
+
   return (
     <div>
       <h1>Sign Up</h1>
@@ -20,17 +21,25 @@ function Signup() {
         <label htmlFor="email">Email</label>
         <input name="email" onChange={handleChange} />
 
+        {errors.email && <div className="error">{errors.email}</div>}
+
         <br />
         <br />
 
         <label htmlFor="password">Password</label>
         <input name="password" onChange={handleChange} />
 
+        {errors.password && <div className="error">{errors.password}</div>}
+
         <br />
         <br />
 
         <label htmlFor="passwordConfirm">Confirm Password</label>
         <input name="passwordConfirm" onChange={handleChange} />
+
+        {errors.passwordConfirm && (
+          <div className="error">{errors.passwordConfirm}</div>
+        )}
 
         <br />
         <br />
